f7b46f796b78d220582512d4b2643b0847be2e47
Rodney Lab <codesign@rodneylab.com>
2022-08-22 07:10:39 +0100

    fix: 💫 check if secret available in astro file

--- src/pages/api.ts
+++ src/pages/api.ts
@@ -2,8 +2,17 @@ import type { APIRoute } from "astro";
 
 export const get: APIRoute = async function get() {
   const secret = import.meta.env.SECRET;
-  const text = secret ? secret.length.toString(10) : "-1"; 
+  const text = secret ? secret.length.toString(10) : "-1";
   return new Response(text, {
+    status: 200,
+  });
+};
+
+export const post: APIRoute = async function post({ request }) {
+  const { secret } = await request.json();
+  const text = secret ? secret.length.toString(10) : "-1";
+  return new Response(text, {
+		headers: { 'content-type': 'application/json' },
     status: 200,
   });
 };
