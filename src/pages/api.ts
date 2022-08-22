e9fdf43e910c968859fb45e8ccfe23eca02948e2
Rodney Lab <codesign@rodneylab.com>
2022-08-22 06:54:50 +0100

    fix: 💫 make more robust

--- src/pages/api.ts
+++ src/pages/api.ts
@@ -2,8 +2,8 @@ import type { APIRoute } from "astro";
 
 export const get: APIRoute = async function get() {
   const secret = import.meta.env.SECRET;
-  const { length } = secret;
-  return new Response(length.toString(10), {
+  const text = secret ? secret.length.toString(10) : "-1"; 
+  return new Response(text, {
     status: 200,
   });
 };
